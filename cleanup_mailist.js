c534f33e203c02ccfdb9b24400abe3840e82caa9
Atom <65351520+atom-tr@users.noreply.github.com>
2024-06-06 09:22:55 +0700

    Update cleanup_mailist.js

--- cleanup_mailist.js
+++ cleanup_mailist.js
@@ -3,6 +3,8 @@ async function cleanSubject() {
   await Promise.all(Array.from(subjectSpans).map((span) => {
     return new Promise((resolve) => {
       setTimeout(() => {
+        if (span.dataset.subjectChecked == 'OK') return
+        span.dataset.subjectChecked = 'OK'
         const cleanedText = span.innerText.replace(
           /^(?!.*Request\sID\s\[#RE-\d+#\]).*(R(e|E):\s)?\[#R(E|e)-\d+#\]\s?\:\s*/g,
           ''
