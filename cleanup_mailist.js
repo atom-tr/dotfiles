d69afd3018c7d292b731132096aa6485c41fcd85
Atom <65351520+atom-tr@users.noreply.github.com>
2024-06-11 13:27:03 +0700

    fixed missing '

--- cleanup_mailist.js
+++ cleanup_mailist.js
@@ -12,7 +12,7 @@ async function cleanSubject() {
         span.innerText = cleanedText;
         const sender = span.parentElement?.parentElement?.nextElementSibling?.firstChild
         if  (/.*((is CRITICAL)|(DOWN)!)/.test(cleanedText)) {
-          if (sender && sender.innerText !== 'Support) sender.append('..  🤬')
+          if (sender && sender.innerText !== 'Support') sender.append('..  🤬')
           if (sender) sender.style.color = "#DC626D"
         } else if ((/.*(((is OK)|(UP)!)|successful)/.test(cleanedText)) || (/(Success on).*/.test(cleanedText))) {
           sender?.append('..  😍')
